897d2284648025874c45823c8554a0b20fb2565d
Claudinei M <claudinei_monteiro@hotmail.com>
2024-07-28 19:18:40 -0300

    correções de textos

--- ola_mundo.py
+++ ola_mundo.py
@@ -1,5 +1,5 @@
 print("olá mundo!") 
 print("Quem bom que você está aqui hoje")
-print("Deus sempre e primeiro lugar")
+print("Deus sempre sempre em primeiro lugar")
 print("Jesus te ama")  
 print("DEUS é amor")
