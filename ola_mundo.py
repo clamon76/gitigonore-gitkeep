3c150bb2f7c649fdfb6d874c94808d33bf0efcef
clamon76 <66097766+clamon76@users.noreply.github.com>
2024-07-28 19:22:42 -0300

    Update ola_mundo.py
    Como exercer print na interface gráfica?

--- ola_mundo.py
+++ ola_mundo.py
@@ -3,3 +3,10 @@ print("Quem bom que você está aqui hoje")
 print("Deus sempre sempre em primeiro lugar")
 print("Jesus te ama")  
 print("DEUS é amor")
+
+from random import randint
+from PyQt5 import QtWidgets, uic
+janela=QtWidgets.QApplication([])
+interface=uic.loadUi ("hello_interface.ui")
+def PRINT():
+    interface.label.setText(str(randint(0,700)))
